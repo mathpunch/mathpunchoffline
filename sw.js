a0876df5c78e7a33a538a493fff7c28f327ccc8a
Mathgod67B <ozzycouts51@gmail.com>
2024-11-29 14:13:25 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,51 @@
-const CACHE_NAME = 'mathpunch-offline-cache-v1';
+const CACHE_NAME = 'mathpunch-v3-cache';
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/manifest.json',
-    '/offline.html'
+    '/offline.html',
+    
+    // Games Section
+    '/kitty/',                
+    '/dog/',                 
+    '/bear/',                
+    '/snake/',               
+    '/shark/',               
+    '/zebra/',               
+    '/turtle/',              
+    '/doggy/',              
+    '/fox/',                
+    '/bat/',                
+    '/wolf/',               
+    '/monkey/',             
+
+    // Emulator Games
+    '/game-ds-mario-kart.zip',
+    '/game-gba-pokemon-red.zip',
+    '/game-gba-lego-star-wars.zip',
+
+    // Other Sections
+    '/crab/',                // MathPunch AI
+    '/requests/',            // Game requests form
+    '/adblocker/',           // Adblocker link
+    '/chatting/',            // Chatting page
+    '/backup-links/',        // Backup links
+    '/new-site-beta/',       // New site beta
+    '/prank-shortcut/',      // Prank shortcut
+
+    // Static Assets (CSS, JS, Images)
+    '/assets/css/style.css',
+    '/assets/js/script.js',
+    '/assets/images/logo.png'
 ];
 
+// Install event - cache all files
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-            .then(() => self.skipWaiting())
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
     );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim();
-});
-
+// Fetch event - serve cached content
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
