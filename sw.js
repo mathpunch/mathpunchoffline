9149bf37d7f89b07cfea0ac8d4d330443c8ccdf5
Mathgod67B <ozzycouts51@gmail.com>
2024-11-29 14:53:51 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,55 +1,82 @@
-const CACHE_NAME = 'mathpunch-v3-cache';
-const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/offline.html',
-    
-    // Games Section
-    '/kitty/',                
-    '/dog/',                 
-    '/bear/',                
-    '/snake/',               
-    '/shark/',               
-    '/zebra/',               
-    '/turtle/',              
-    '/doggy/',              
-    '/fox/',                
-    '/bat/',                
-    '/wolf/',               
-    '/monkey/',             
+const cacheName = 'offline-cache-v1';
+const filesToCache = [
+  '/', // Cache the homepage
+  'index.html',
+  'sw.js',
+  'https://mathpunch.github.io/kitty/',
+  'https://mathpunch.github.io/dog/',
+  'https://mathpunch.github.io/bear/',
+  'https://mathpunch.github.io/snake/',
+  'https://mathpunch.github.io/shark/',
+  'https://mathpunch.github.io/zebra/',
+  'https://mathpunch.github.io/turtle/',
+  'https://mathpunch.github.io/doggy/',
+  'https://mathpunch.github.io/fox/',
+  'https://mathpunch.github.io/bat/',
+  'https://mathpunch.github.io/wolf/',
+  'https://mathpunch.github.io/monkey/',
+  'https://mathpunch.github.io/flashy/Mario%20Kart.zip',
+  'https://mathpunch.github.io/Licky/Pokemon%20Red%20Fire.zip',
+  'https://mathpunch.github.io/didactic-fortnight/LEGO%20Star%20Wars%20II.zip',
+  'https://mathpunch.github.io/ixlmathgg/games/kitty',
+  'https://mathpunch.github.io/ixlmathgg/games/dog',
+  'https://mathpunch.github.io/ixlmathgg/games/bear',
+  'https://mathpunch.github.io/ixlmathgg/games/snake',
+  'https://mathpunch.github.io/ixlmathgg/games/shark',
+  'https://mathpunch.github.io/ixlmathgg/games/zebra',
+  'https://mathpunch.github.io/ixlmathgg/games/turtle',
+  'https://mathpunch.github.io/ixlmathgg/games/doggy',
+  'https://mathpunch.github.io/ixlmathgg/games/fox',
+  'https://mathpunch.github.io/ixlmathgg/games/bat',
+  'https://mathpunch.github.io/ixlmathgg/games/wolf',
+  'https://mathpunch.github.io/ixlmathgg/games/monkey',
+  'https://mathpunch.github.io/ixlmathgg/flashy/Mario%20Kart.zip',
+  'https://mathpunch.github.io/ixlmathgg/Licky/Pokemon%20Red%20Fire.zip',
+  'https://mathpunch.github.io/ixlmathgg/didactic-fortnight/LEGO%20Star%20Wars%20II.zip',
+  // Add more URLs for your games, images, and other resources as needed
 
-    // Emulator Games
-    '/game-ds-mario-kart.zip',
-    '/game-gba-pokemon-red.zip',
-    '/game-gba-lego-star-wars.zip',
-
-    // Other Sections
-    '/crab/',                // MathPunch AI
-    '/requests/',            // Game requests form
-    '/adblocker/',           // Adblocker link
-    '/chatting/',            // Chatting page
-    '/backup-links/',        // Backup links
-    '/new-site-beta/',       // New site beta
-    '/prank-shortcut/',      // Prank shortcut
-
-    // Static Assets (CSS, JS, Images)
-    '/assets/css/style.css',
-    '/assets/js/script.js',
-    '/assets/images/logo.png'
+  // You may also want to cache static assets like images, stylesheets, and scripts
+  'https://mathpunch.github.io/style.css',
+  'https://mathpunch.github.io/script.js',
+  'https://mathpunch.github.io/image1.jpg',
+  'https://mathpunch.github.io/image2.png',
+  'https://mathpunch.github.io/assets/some-other-file.js',
+  // Add more static assets or pages here as needed
 ];
 
-// Install event - cache all files
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-// Fetch event - serve cached content
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        }).catch(() => caches.match('/offline.html'))
-    );
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [cacheName];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // If there's a cached version, return it
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // Otherwise, fetch the content from the network
+      return fetch(event.request);
+    })
+  );
 });
